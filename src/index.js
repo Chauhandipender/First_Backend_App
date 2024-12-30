2d41bebc5b9455d0f45bb9e00592625392fbd25f
Chauhandipender <dipender0901@gmail.com>
2024-12-30 19:36:37 +0530

    writing api response and error

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import dotenv from "dotenv"
 dotenv.config()
 
 connectDB()
+.then(() => {
+    app.litsen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB conenction failed : ", error);
+})
